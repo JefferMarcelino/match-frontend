cfc29b397388af2b73544b1267de01fac2791d95
JefferMarcelino <jeffersunde72@gmail.com>
2022-08-07 21:26:01 +0200

    Scrool to top button

---
+++ src/components/ScroolToTop.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import { ArrowUp } from "phosphor-react"
+
+export const ScroolToTop = () => {
+    const [ showButton, setShowButton ] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            setShowButton(window.scrollY >= 10)
+        })
+    }, [])
+
+    return(
+        <button 
+        className={`bg-link rounded-full inline-block p-2 bottom-2 right-2 ${ showButton ? "fixed" : "none hidden" }`}
+        onClick={() => {
+            window.scrollTo(0, 0)
+        }}
+        >
+            <ArrowUp size={35} />
+        </button>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ApolloProvider } from "@apollo/client"
 import type { AppProps } from 'next/app'
 import { client } from "../lib/apollo"
 import '../public/styles/globals.css'
+import { ScroolToTop } from "../components/ScroolToTop"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className="w-full h-full min-h-screen text-blue text-lg p-5">
         <div className='max-w-4xl mx-auto flex flex-col gap-10'>
           <Component {...pageProps} />
+          <ScroolToTop />
         </div>
       </div>
     </ApolloProvider>
